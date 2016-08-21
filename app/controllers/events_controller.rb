01cfec796749464f5ae958e5b1c420c2c442a2eb
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-08-21 14:02:28 +0100

    Reponse code on save event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,11 +7,14 @@ class EventsController < ApplicationController
     date = timestamp_to_date data["Timestamp"]
     email_type = EmailType.where(name: data["EmailType"]).first_or_create
     event = Event.new(address: data["Address"],
-                 email_type: email_type,
-                 event_type: data["Event"],
-                 timestamp: date)
-
-    event.save
+                      email_type: email_type,
+                      event_type: data["Event"],
+                      timestamp: date)
+    if event.save
+      render plain: "OK", status: :ok
+    else
+      render plain: "ERROR", status: :internal_server_error
+    end
   end
 
 private
